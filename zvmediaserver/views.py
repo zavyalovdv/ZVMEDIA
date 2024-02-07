e402e83d8500ead8f4a0f8786cb4db17a98b1f22
Zavyalov Daniil <daniilzavialov@yandex.ru>
2024-02-08 01:17:58 +0300

    First deploy

---
+++ zvmediaserver/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+  
+def index(request):
+    return HttpResponse("Главная")
+ 
+def about(request):
+    return HttpResponse("О сайте")
+ 
+def contact(request):
+    return HttpResponse("Контакты")
